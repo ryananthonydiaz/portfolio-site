45b59792482a5470a467fec8ca27265d8e7e8cb9
Ryan Diaz <ryandiaz@Ryans-MBP.lan1>
2020-07-03 23:37:04 -0700

    Contact Page completely styled

--- src/unauth/Contact.jsx
+++ src/unauth/Contact.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import UnauthDesktopDrawer from '../navigation/UnauthDesktopDrawer';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -8,6 +10,20 @@ const useStyles = makeStyles(theme => ({
   formContainer: {
     width: '100%',
     margin: theme.spacing(6, 0),
+  },
+  paper: {
+    margin: theme.spacing(0, 0, 5, 0),
+    width: theme.spacing(40),
+    height: theme.spacing(8),
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paperItem: {
+    fontSize: '2rem',
+    fontWeight: '100',
+    letterSpacing: '.5rem',
+    color: '#757575'
   },
   formItem: {
     width: '400px',
@@ -38,14 +54,21 @@ function Contact() {
       container
     >
       <Grid item>
-          <TextField
-            label="Email"
-            variant="outlined"
-            onChange={() => null}
-            className={classes.formItem}
-          />
+        <Paper elevation={5} className={classes.paper}>
+          <div className={classes.paperItem}>CONTACT ME</div>
+        </Paper>
       </Grid>
       <Grid item>
+        <InputLabel>Email</InputLabel>
+        <TextField
+          label="Email"
+          variant="outlined"
+          onChange={() => null}
+          className={classes.formItem}
+        />
+      </Grid>
+      <Grid item>
+        <InputLabel>Message</InputLabel>
         <TextField
             multiline
             rows={10}
