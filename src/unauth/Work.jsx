213851fdf6ba5980f275dd5422c6796e2820332e
Ryan Diaz <ryandiaz@Ryans-MBP.lan1>
2020-07-04 00:27:40 -0700

    Apt Health Gif added to projects

--- src/unauth/Work.jsx
+++ src/unauth/Work.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import AptHealth from '../assets/apt-health.gif';
 import UnauthDesktopDrawer from '../navigation/UnauthDesktopDrawer';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+
+// ALL following imports may need to be removed
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
   formContainer: {
@@ -32,11 +47,90 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     color: '#FFD700',
-  }
+  },
+
+  // Everythin below in styles may need to be taken out
+
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
 }));
 
 function Work() {
   const classes = useStyles();
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  // This will have to be on the server and called upon site opening up and then cached to
+  // local storage
+  const projectContent = [
+    {cardHeaderTitle: 'Apt Health',
+    cardHeaderSubHeader: 'October 2019 - April 2020',
+    cardMediaImage: AptHealth,
+    cardMediaTitle: 'Apt Health Landing Page',
+    cardDescription: `This project I was apprroached by a startup to build a web application in React.js
+                      translated from an in-production iOS applicaiton that was a platform for doctors
+                      and physicians alike to communicate with patients and incentivize patients in
+                      staying current with medical appointments.`,
+    },
+  ]
+
+  const projectGrid = projectContent.map(({
+      cardHeaderTitle,
+      cardHeaderSubHeader,
+      cardMediaImage,
+      cardMediaTitle,
+      cardDescription,
+    }) => (
+    <Card raised className={classes.root}>
+      <CardHeader
+        avatar={<Avatar>R</Avatar>}
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon /> 
+            {/* This button can route the user to a page where this particular project can solo */}
+          </IconButton>
+        }
+        title={cardHeaderTitle}
+        subheader={cardHeaderSubHeader}
+      />
+      <CardMedia
+        className={classes.media}
+        image={cardMediaImage}
+        title={cardMediaTitle}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {cardDescription}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+          {/* This button (another button) can ask the user if they want to message me about this
+          project? */}
+        </IconButton>
+      </CardActions>
+    </Card>));
+
   const content = (
     <Grid
       justify="center"
@@ -50,18 +144,7 @@ function Work() {
           <div className={classes.paperItem}>PROJECTS</div>
         </Paper>
       </Grid>
-
-      <Grid item>
-        {/* text field went here */}
-      </Grid>
-
-      <Grid item>
-        {/* text field went here */}
-      </Grid>
-      
-      <Grid item>
-      {/* button went here */}
-      </Grid>
+      {projectGrid}
     </Grid>
   );
 
